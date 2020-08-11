0ae6c613bc9f225212d0841780874fbe08b2369b
joshmorel <morel.josh@hotmail.com>
2020-08-11 16:32:42 -0400

    add posts pagination to realm

--- posts-moderation/realm-config/services/http-service/incoming_webhooks/get-posts/source.js
+++ posts-moderation/realm-config/services/http-service/incoming_webhooks/get-posts/source.js
@@ -1,20 +1,29 @@
 // This function is the webhook's request handler.
 exports = function(payload, response) {
-    const mongodb = context.services.get("mongodb-atlas");
-    const posts = mongodb.db("fightpandemics").collection("posts");
-    
-    return posts.aggregate([
-      // exclude sourced by fightpandemics
-      { $match: { airtableId: { $exists: false } } },
-      { $sort: { _id: -1 } },
-      { $project: {
-        _id: 0,
-        link: { $concat: [ context.values.get("baseUrl"), "/post/", { $toString: "$_id" } ] },
-        name: 1,
-        title: 1,
-        content: 1,
-        createdAt: { $toString: "$createdAt" },
-        updatedAt: { $toString: "$updatedAt" }
-      } }
-    ]).toArray();
+  let {skip, limit} = payload.query;
+  skip = parseInt(skip, 10);
+  limit = parseInt(limit, 10);
+
+  if (isNaN(skip)) skip = 0;
+  if (isNaN(limit)) limit = 100;
+
+  const mongodb = context.services.get("mongodb-atlas");
+  const posts = mongodb.db("fightpandemics").collection("posts");
+  
+  return posts.aggregate([
+    // exclude sourced by fightpandemics
+    { $match: { airtableId: { $exists: false } } },
+    { $sort: { _id: -1 } },
+    { $project: {
+      _id: 0,
+      link: { $concat: [ context.values.get("baseUrl"), "/post/", { $toString: "$_id" } ] },
+      name: 1,
+      title: 1,
+      content: 1,
+      createdAt: { $toString: "$createdAt" },
+      updatedAt: { $toString: "$updatedAt" }
+    } },
+    { $skip: skip },
+    { $limit: limit }
+  ]).toArray();
 };
